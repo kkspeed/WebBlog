54d19b113bf956f931a87c2c91e7be784ea27070
bruce <leilmyxwz@gmail.com>
2015-02-25 03:20:25 -0500

    Initial commit on skeleton

--- haskell/WebBlog/Rules.hs
+++ haskell/WebBlog/Rules.hs
@@ -1,1 +1,19 @@
- 
+{-# LANGUAGE OverloadedStrings #-}
+module WebBlog.Rules where
+
+import Hakyll
+
+compileRules :: Rules ()
+compileRules = do
+  compileImage
+  compileCss
+
+compileImage :: Rules ()
+compileImage = match "images/*" $ do
+                 route idRoute
+                 compile copyFileCompiler
+
+compileCss :: Rules ()
+compileCss = match "css/*" $ do
+               route idRoute
+               compile copyFileCompiler
