df25ce1607492a0c51cfa932f1351077fd1099dd
Bruce Li <leilmyxwz@gmail.com>
2015-02-25 15:51:27 -0500

    Get compilation to work

--- haskell/WebBlog/Rules.hs
+++ haskell/WebBlog/Rules.hs
@@ -1,12 +1,15 @@
 {-# LANGUAGE OverloadedStrings #-}
 module WebBlog.Rules where
 
+import Data.Monoid
 import Hakyll
 
 compileRules :: Rules ()
 compileRules = do
   compileImage
   compileCss
+  compilePosts
+  compileTemplates
 
 compileImage :: Rules ()
 compileImage = match "images/*" $ do
@@ -17,3 +20,19 @@ compileCss :: Rules ()
 compileCss = match "css/*" $ do
                route idRoute
                compile copyFileCompiler
+
+compilePosts :: Rules ()
+compilePosts = match "posts/*" $ do
+        route $ setExtension "html"
+        compile $ do
+          pandocCompiler
+            >>= loadAndApplyTemplate "templates/post.html" postCtx
+            >>= loadAndApplyTemplate "templates/base.html" postCtx
+            >>= relativizeUrls
+
+compileTemplates :: Rules ()
+compileTemplates = match "templates/*" $ compile templateCompiler
+
+postCtx :: Context String
+postCtx = dateField "date" "%B %e, %Y" <>
+          defaultContext
