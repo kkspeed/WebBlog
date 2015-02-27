1deb528744a6c94cc45df9d733bed64b4b1b8ba0
Bruce Li <leilmyxwz@gmail.com>
2015-02-27 14:30:16 -0500

    Updated deploy commands

--- haskell/Main.hs
+++ haskell/Main.hs
@@ -4,4 +4,9 @@ import Hakyll
 import WebBlog.Rules
 
 main :: IO ()
-main = hakyll compileRules
+main = hakyllWith config compileRules
+
+config :: Configuration
+config = defaultConfiguration {
+           deployCommand = "rsync -ave ssh _site/ bruce@bruce-blog:/var/www/html/"
+         }
